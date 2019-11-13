77a0ead0c47ea6a73a0324264bfb83efd99ed82e
megamind <taweesak.in@gmail.com>
2019-11-13 22:32:22 +0700

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery_ujs
 //= require rails-ujs
+//= require bootstrap-sprockets
 //= require bootstrap.min  
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require bootstrap-sprockets
+
