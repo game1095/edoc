05e6820643a0fc3ac6f86a144f83f22d872896d2
game1095 <taweesak.in@gmail.com>
2019-12-07 19:47:37 +0700

    sent document v2

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ApplicationRecord
   has_many :user
-  # has_and_belongs_to_many :sent , optional: true
-  has_and_belongs_to_many :document , optional: true
-
+  belongs_to :sent , optional: true
+  # has_many :document
+  # has_and_belongs_to_many:document , optional: true
 end
---
+++ db/migrate/20191023055735_create_sents.rb
@@ -0,0 +1,8 @@
+class CreateSents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sents do |t|
+
+      t.timestamps
+    end
+  end
+end
